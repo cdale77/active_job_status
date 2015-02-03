458ccfbcde5c31ae9aac919f68aaae1758159cfb
Brad Johnson <cdale77@gmail.com>
2015-02-02 16:18:02 -0800

    wip

--- lib/active_job_status/trackable_job.rb
+++ lib/active_job_status/trackable_job.rb
@@ -1,25 +1,16 @@
 require "active_job"
 class TrackableJob < ActiveJob::Base
 
-  attr_reader :status
-
   before_enqueue do |job|
     puts "before enqueue"
-    @status = :queuing
-  end
-
-  after_enqueue do |job|
-    @status = :queued
   end
 
   before_perform do |job|
     puts "before perform"
-    @status = :working
   end
 
   after_perform do |job|
     puts "after perform"
-    @status = :complete
   end
 end
 
