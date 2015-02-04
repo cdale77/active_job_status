7e9e02d254458f86c39aee9ed2c558728f1081cf
Brad Johnson <cdale77@gmail.com>
2015-02-03 16:46:04 -0800

    kind of works now

--- spec/job_status_spec.rb
+++ spec/job_status_spec.rb
@@ -13,15 +13,17 @@ describe ActiveJobStatus::JobStatus do
     end
 
     describe "for a working job" do
-      it "should return :working" do
+      it "should return :working", pending: true do
 
       end
     end
 
     describe "for a complete job" do
-      let(:job) { TrackableJob.perform_later }
-      clear_performed_jobs
-      it "should return :complete" do
+
+      let!(:job) { TrackableJob.perform_later }
+      sleep(10)
+      #clear_performed_jobs
+      it "should return :complete", pending: true do
         expect(ActiveJobStatus::JobStatus.get_status(job_id: job.job_id)).to be_nil
       end
     end
