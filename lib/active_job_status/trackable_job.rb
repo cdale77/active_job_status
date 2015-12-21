f838609ea3a92b4e4197f5e3dec81b1c783afe28
Brad Johnson <cdale77@gmail.com>
2015-12-21 11:20:00 -0800

    better namespacing

--- lib/active_job_status/trackable_job.rb
+++ lib/active_job_status/trackable_job.rb
@@ -1,11 +1,11 @@
 require "active_job"
-class ActiveJobStatus::TrackableJob < ActiveJob::Base
+module ActiveJobStatus
+  class TrackableJob < ActiveJob::Base
 
-  before_enqueue { ActiveJobStatus::JobTracker.enqueue(job_id: @job_id) }
+    before_enqueue { ActiveJobStatus::JobTracker.enqueue(job_id: @job_id) }
 
-  before_perform { ActiveJobStatus::JobTracker.update(job_id: @job_id, status: :working) }
+    before_perform { ActiveJobStatus::JobTracker.update(job_id: @job_id, status: :working) }
 
-  after_perform { ActiveJobStatus::JobTracker.remove(job_id: @job_id) }
+    after_perform { ActiveJobStatus::JobTracker.remove(job_id: @job_id) }
+  end
 end
-
-
