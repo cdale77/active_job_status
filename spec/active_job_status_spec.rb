4ddbc988ed8eeb984ba59861d6eee547647e06af
kami-zh <h.zenigami@everyleaf.com>
2017-03-28 12:29:21 +0900

    Raise error when store has not set

---
+++ spec/active_job_status_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe ActiveJobStatus do
+  describe '.store' do
+    it 'raises error when store has not set' do
+      expect { ActiveJobStatus.store }.to raise_error(ActiveJobStatus::NoStoreError)
+    end
+
+    context 'when store has set' do
+      let(:store) { new_store }
+
+      before do
+        ActiveJobStatus.store = store
+      end
+
+      it 'returns configured store' do
+        expect { ActiveJobStatus.store }.to_not raise_error
+        expect(ActiveJobStatus.store).to eq store
+      end
+    end
+  end
+end
+
